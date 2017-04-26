84b55dd9c62805aba05fd218a525f8363d8c6111
松根佑眞 <yuma.matsune@gmail.com>
2017-04-26 23:52:47 +0900

    Init

---
+++ IndexableDictionary/IndexableDictionary.h
@@ -0,0 +1,19 @@
+//
+//  IndexableDictionary.h
+//  IndexableDictionary
+//
+//  Created by Yuma Matsune on 2017/04/26.
+//  Copyright © 2017年 matsune. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for IndexableDictionary.
+FOUNDATION_EXPORT double IndexableDictionaryVersionNumber;
+
+//! Project version string for IndexableDictionary.
+FOUNDATION_EXPORT const unsigned char IndexableDictionaryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IndexableDictionary/PublicHeader.h>
+
+
