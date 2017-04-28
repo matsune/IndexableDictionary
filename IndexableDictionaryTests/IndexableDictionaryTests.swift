d9285e93771307bec828aaa056c61e38f2160038
Yuma Matsune <yuma.matsune@gmail.com>
2017-04-28 17:30:51 +0900

    Added unit tests

--- IndexableDictionaryTests/IndexableDictionaryTests.swift
+++ IndexableDictionaryTests/IndexableDictionaryTests.swift
@@ -11,26 +11,54 @@ import XCTest
 
 class IndexableDictionaryTests: XCTestCase {
     
+    var idxDictionary = IndexableDictionary<String, Int>()
+    
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        idxDictionary = [("a", 1), ("b", 2), ("c", 3)]
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testSubscriptPosition() {
+        XCTAssertEqual(idxDictionary[0].key, "a")
+        XCTAssertEqual(idxDictionary[1].key, "b")
+        XCTAssertEqual(idxDictionary[2].key, "c")
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testSubscriptBounds() {
+        XCTAssertEqual(idxDictionary[0..<3].map {$0.key}, ["a", "b", "c"])
     }
     
+    func testSubscriptKey() {
+        XCTAssertEqual(idxDictionary["a"], 1)
+        XCTAssertEqual(idxDictionary["b"], 2)
+        XCTAssertEqual(idxDictionary["c"], 3)
+    }
+    
+    func testIndexForKey() {
+        XCTAssertEqual(idxDictionary.index(forKey: "a"), 0)
+        XCTAssertEqual(idxDictionary.index(forKey: "b"), 1)
+        XCTAssertEqual(idxDictionary.index(forKey: "c"), 2)
+    }
+    
+    func testKeys() {
+        XCTAssertEqual(idxDictionary.keys.map {$0}, ["a", "b", "c"])
+    }
+    
+    func testValues() {
+        XCTAssertEqual(idxDictionary.values.map {$0}, [1, 2, 3])
+    }
+    
+    func testRemoveValueForKey() {
+        XCTAssertEqual(idxDictionary.removeValue(forKey: "a"), 1)
+        XCTAssertEqual(idxDictionary.removeValue(forKey: "a"), nil)
+    }
+    
+    func testUpdateValue() {
+        XCTAssertEqual(idxDictionary.updateValue(100, forKey: "a"), 1)
+        XCTAssertEqual(idxDictionary.updateValue(100, forKey: "z"), nil)
+    }
 }
